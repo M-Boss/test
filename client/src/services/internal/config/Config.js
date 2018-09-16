5fba0d44ba17bb64a8a4c388695db87da3434e9d
guy <arbab.mohammad92@gmail.com>
2018-09-16 15:49:31 +0430

    Menu close button

--- client/src/services/internal/config/Config.js
+++ client/src/services/internal/config/Config.js
@@ -2,10 +2,10 @@ import _ from 'lodash'
 
 const config = {
     app: {
-        // api_root: 'http://54.251.129.242/api/',
-        // assets: 'http://54.251.129.242/assets/',
-        api_root: 'http://localhost:8080/api/',
-        assets: 'http://localhost:8080/assets/',
+        api_root: 'http://54.251.129.242/api/',
+        assets: 'http://54.251.129.242/assets/',
+        // api_root: 'http://localhost:8080/api/',
+        // assets: 'http://localhost:8080/assets/',
     }
 };
 
