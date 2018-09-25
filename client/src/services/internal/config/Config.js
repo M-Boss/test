183b8c270013dba9ca855703451d15dc208f0c82
guy <arbab.mohammad92@gmail.com>
2018-09-26 00:56:50 +0330

    website slug photo ratio

--- client/src/services/internal/config/Config.js
+++ client/src/services/internal/config/Config.js
@@ -2,8 +2,10 @@ import _ from 'lodash'
 
 const config = {
     app: {
-        api_root: 'http://54.251.129.242/api/',
-        assets: 'http://54.251.129.242/assets/',
+        // api_root: 'http://54.251.129.242/api/',
+        // assets: 'http://54.251.129.242/assets/',
+        api_root: 'https://nikahku.com/api/',
+        assets: 'https://nikahku.com/assets/',
         // api_root: 'http://localhost:8080/api/',
         // assets: 'http://localhost:8080/assets/',
     }
